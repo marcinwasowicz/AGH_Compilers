c68a4c9e3e35f3bb094fc9bfea6297695261eb1b
marcinwasowicz <marcinwasowicz@LAPTOP-7TT223E8.localdomain>
2020-11-30 18:42:07 +0100

    Printing AST in main.py script

--- Matrix_Language_Project/main.py
+++ Matrix_Language_Project/main.py
@@ -1,8 +1,10 @@
 import sys
 import scanner
 import parser
+import TreePrinter
 
 if __name__ == '__main__':
 
     text = open(sys.argv[1]).read()
-    parser.parser.parse(input=text, lexer=scanner.lexer)
+    ast = parser.parser.parse(input=text, lexer=scanner.lexer)
+    ast.printTree()
