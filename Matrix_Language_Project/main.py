0387b77ec9fc64208d92001c3b1add700a4c4a6b
marcinwasowicz <marcinwasowicz@LAPTOP-7TT223E8.localdomain>
2020-12-30 17:28:52 +0100

    script is now generating c code

--- Matrix_Language_Project/main.py
+++ Matrix_Language_Project/main.py
@@ -3,14 +3,22 @@ from frontend import type_checker
 from frontend import scanner
 from frontend import parser
 from frontend import tree_printer
+from c_code_generation import generator
 
 import sys
 
 
 if __name__ == '__main__':
-    text = open(sys.argv[1]).read()
+    text = open(sys.argv[1], 'r').read()
     ast = parser.parser.parse(input=text, lexer=scanner.lexer, tracking=True)
     type_checker = type_checker.TypeChecker(symbol_table.SymbolTable())
     errors = type_checker.visit(ast)
-    for error in errors:
-        print(error)
+    if errors:
+        for error in errors:
+            print(error)
+    else:
+        code_generator = generator.CodeGenerator()
+        c_code = code_generator.generate(ast)
+        open(sys.argv[2], 'w+').write(c_code)
+
+        
